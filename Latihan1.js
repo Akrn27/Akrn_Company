daf917d06b73db4cf32d9beb4cc0aa10619b868e
Muhammad Andika <akurnia755@gmail.com>
2021-03-04 22:35:32 +0700

    update html and css

--- Latihan1.js
+++ Latihan1.js
@@ -1,10 +1,3 @@
-var angka = prompt('Masukkan angka:');
-if(angka % 2 === 0){
-    alert(angka + ' adalah bilangan GENAP');
-}else{
-    alert(angka + ' adalah bilangan GANJIL');
-}
-
 function ads(){
     alert('Your Computer has a virus..')
 }
